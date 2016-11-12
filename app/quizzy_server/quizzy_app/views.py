40a6e0fdb893edabc7fcbecadd08b01447a82d82
Lucas Davis <LucasDavis@dcc-wl-445.dynamic2.rpi.edu>
2016-11-12 18:03:17 -0500

    adds template and static framework to the quizzy django app

--- app/quizzy_server/quizzy_app/views.py
+++ app/quizzy_server/quizzy_app/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+## Create your views here.
+#from django.http import HttpResponse
+
+
+#def index(request):
+    #return HttpResponse("Hello, world.")
+    
 from django.http import HttpResponse
+from django.template import loader
+
+#from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('quizzy_app/index.html')
+    context = {
+        #'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
--- app/quizzy_server/quizzy_server/settings.py
+++ app/quizzy_server/quizzy_server/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'quizzy_server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
