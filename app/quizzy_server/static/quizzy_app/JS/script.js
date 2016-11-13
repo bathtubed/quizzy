ebaae18621402eecf5f6a76f951af01b07616a83
EdWard <ezeward4@gmail.com>
2016-11-13 03:31:44 -0500

    Adds line numbers to mirrorcode

--- app/quizzy_server/static/quizzy_app/JS/script.js
+++ app/quizzy_server/static/quizzy_app/JS/script.js
@@ -4,7 +4,8 @@ $(document).ready(function(){
     console.log("Initializing...");
     $(".free_response textarea").each(function(i, obj)
     {
-        console.log("Adding text area: " + obj.name);
-        code_mirrors.push(CodeMirror.fromTextArea(obj));
+        console.log("Adding text area: " + obj.name + " of language " + language);
+        var mirror = CodeMirror.fromTextArea(obj, {mode: {name: language}, lineNumbers: true});
+        code_mirrors.push(mirror);
     });
 });
