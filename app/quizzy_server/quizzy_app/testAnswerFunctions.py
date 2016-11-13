86bb872ab222aa41c1504e9f48716840cf3a2682
Shashank98 <shashanksundar@dcc-wl-436.dynamic2.rpi.edu>
2016-11-12 23:29:53 -0500

    Exam class and functions

--- app/quizzy_server/quizzy_app/testAnswerFunctions.py
+++ app/quizzy_server/quizzy_app/testAnswerFunctions.py
@@ -10,3 +10,9 @@ str3 = "333333333333333333333"
 updateAns(root, exam, studentNames[0], 0, str1)
 updateAns(root, exam, studentNames[0], 0, str2)
 updateAns(root, exam, studentNames[0], 0, str3)
+
+print (getExam(root, 'exam1')).questions['0']
+print (getExam(root, 'exam1')).questions['1']
+print (getExam(root, 'exam1')).questions['2']
+print (getExam(root, 'exam1')).start_time
+print (getExam(root, 'exam1')).end_time
