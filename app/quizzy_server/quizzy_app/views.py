622db3a4adce07a70ed6703cd1ccba7a0ac6807d
Lucas Davis <LucasDavis@dcc-wl-445.dynamic2.rpi.edu>
2016-11-13 00:37:12 -0500

    creates the server files and script files to run basic code

--- app/quizzy_server/quizzy_app/views.py
+++ app/quizzy_server/quizzy_app/views.py
@@ -1,22 +1,30 @@
-#from django.shortcuts import render
-
-## Create your views here.
-#from django.http import HttpResponse
-
-
-#def index(request):
-    #return HttpResponse("Hello, world.")
-    
 from django.http import HttpResponse
 from django.template import loader
-
-#from .models import Question
-
+import quizzy_app.answerFunctions as exam_lib
+from django.conf import settings
+import datetime
+import calendar
 
 def index(request):
-    #latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('quizzy_app/index.html')
-    context = {
-        #'latest_question_list': latest_question_list,
-    }
+    exam_data = exam_lib.getExam(settings.BASE_DIR + '/data',\
+                                 request.GET['examID'])
+    start_time = exam_data.start_time
+    end_time = exam_data.end_time
+    server_time = datetime.datetime.now()
+    
+    if server_time < start_time:
+        displayed_start = start_time
+        template = loader.get_template('quizzy_app/waitingpage.html')
+        context = {
+            'start_time':displayed_start,
+            'exam_name':exam_data.ID
+        }
+         
+    else:
+        exam_object = exam_data
+        template = loader.get_template('quizzy_app/index.html')
+        context = {
+            'exam':exam_object
+        }
+        
     return HttpResponse(template.render(context, request))
